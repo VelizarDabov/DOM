5b8eb6a906b459ef099941546d7f50d67dab5261
VelizarDabov <112800258+VelizarDabov@users.noreply.github.com>
2023-01-25 14:57:04 +0200

    Add files via upload

---
+++ 02.Collect List Items/CollectListItems.js
@@ -0,0 +1,8 @@
+function extractText() {
+  let elements = document.querySelectorAll('ul#items li');
+  let textArea = document.querySelector('#result')
+
+  for(let node of elements) {
+    textArea.value += node.textContent + '\n'
+  }
+}
